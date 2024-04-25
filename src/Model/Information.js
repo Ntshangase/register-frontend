83f6df12a4299f3efe1a0f355484f3080fabe421
Ntshangase <simphiwe323i@gmail.com>
2024-04-25 06:36:20 +0200

    fetch.response

--- src/Model/Information.js
+++ src/Model/Information.js
@@ -1,12 +1,16 @@
-
 function Information() {
-    console.log("Inside but not rendering ");
+	// console.log("This code must run twice: ");
 
-    return (
-        <h1>Hello world!</h1>
-    )
+	return <h1>Hello world!</h1>;
 }
 
+// console.log("This code must run once");
+fetch("https://jsonplaceholder.typicode.com/todos").then((response) => {
+	if(response.ok){ 
+        console.log("status: "+ response.status);
+        console.log(response);
 
+    }else {console.log("An error occured while fetching response")}
+});
 
 export default Information;
