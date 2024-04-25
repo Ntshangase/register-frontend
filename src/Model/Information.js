5ece48bbe3eaa017701f441bece65e8395090312
Ntshangase <simphiwe323i@gmail.com>
2024-04-25 07:42:15 +0200

    undefined data

--- src/Model/Information.js
+++ src/Model/Information.js
@@ -5,12 +5,17 @@ function Information() {
 }
 
 // console.log("This code must run once");
-fetch("https://jsonplaceholder.typicode.com/todos").then((response) => {
-	if(response.ok){ 
-        console.log("status: "+ response.status);
-        console.log(response);
-
-    }else {console.log("An error occured while fetching response")}
-});
+fetch("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json")
+	.then(response => {
+		if (response.ok) {
+			console.log("status: " + response.status);
+			console.log(response);
+		} else {
+			console.log("An error occured while fetching response");
+		}
+	})
+	.then((data) => {
+		console.log(data);
+	});
 
 export default Information;
