ad9044d003974126f9f56fa6860d9da7ca7602a1
Ntshangase <simphiwe323i@gmail.com>
2024-05-15 07:54:47 +0200

    useState

--- src/Model/Information.js
+++ src/Model/Information.js
@@ -1,22 +1,39 @@
-function Information() {
-	// console.log("This code must run twice: ");
+import { useState } from "react";
 
-	return <h1>Hello world!</h1>;
+function Information() {
+	let [currentPlate, setPlate] = useState("BB 78 ZN");
+
+	//change numberPlate
+	//let x = 0;
+	//const arr = ["BB 79 ZN", "BC 80 ZN", "BD 81 ZN", "BF 82 ZN", "BG 83 ZN", "BH 83 ZN"];
+
+	//wrap inside function
+	function updatePlate() {
+		setPlate("him zn");
+	}
+
+	return (
+		<>
+			<p>car 1: {currentPlate}</p>
+
+			<button onClick={ () => {updatePlate()}}>New Plate</button>
+		</>
+	);
 }
 
 // console.log("This code must run once");
-fetch("http://localhost:8080/users")
-	.then(response => {
-		if (response.ok) {
-			console.log("status: " + response.status);
-			console.log(response);
-			return response.json();
-		} else {
-			console.log("An error occured while fetching response");
-		}
-	})
-	.then((data) => {
-		console.log(data);
-	});
+// fetch("http://localhost:8080/users")
+// 	.then(response => {
+// 		if (response.ok) {
+// 			console.log("status: " + response.status);
+// 			console.log(response);
+// 			return response.json();
+// 		} else {
+// 			console.log("An error occured while fetching response");
+// 		}
+// 	})
+// 	.then((data) => {
+// 		console.log(data);
+// 	});
 
 export default Information;
