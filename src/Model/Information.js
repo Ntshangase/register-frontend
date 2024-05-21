c68c9644bb87dad38280e2502f2eb98d13914753
Ntshangase <simphiwe323i@gmail.com>
2024-05-21 07:48:48 +0200

    api data

--- src/Model/Information.js
+++ src/Model/Information.js
@@ -1,58 +1,39 @@
 import { useState } from "react";
 
 function Information() {
-	const [currentCount, setCount] = useState([
-		{ name: "simphiwe", course: "mathematics", id: 2268 },
-		{ name: "Thandeka", course: "English", id: 2345 },
-	]);
+	const [currentData, setData] = useState([]);
 
-	//change numberPlate
-	console.log(currentCount);
-
-	//wrap inside function
-	function updateCount() {
-		setCount((prevState) => ([...prevState, {name:"Joshua", course:"Physics", id: 2300}]));
-	}
+	//fetch data from API
+	console.log("This code must run once");
+	fetch("http://localhost:8080/users")
+		.then((response) => {
+			if (response.ok) {
+				console.log("status: " + response.status);
+				console.log(response);
+				return response.json();
+			} else {
+				console.log("An error occured while fetching response");
+			}
+		})
+		.then((data) => {
+			console.log(data);
+			setData(data);
+		});
 
 	return (
 		<>
-			<button
-				onClick={() => {
-					updateCount();
-				}}
-			>
-				Update
-			</button>
-
-			<h1>Render elements</h1>
 			<div>
-				{currentCount.map((item) => (
-					<div key={item.id}>
-						<h6>{item.name}</h6>
-						<h6>{item.course}</h6>
-						<p>{item.id}</p>
-						<p></p>
+				<h1>Home renovations</h1>
+			</div>
+			<div>
+				{currentData.map((item) => (
+					<div key={item.name}>
+						<p>{item.name}</p>
 					</div>
 				))}
 			</div>
-			
 		</>
 	);
 }
-
-// console.log("This code must run once");
-// fetch("http://localhost:8080/users")
-// 	.then(response => {
-// 		if (response.ok) {
-// 			console.log("status: " + response.status);
-// 			console.log(response);
-// 			return response.json();
-// 		} else {
-// 			console.log("An error occured while fetching response");
-// 		}
-// 	})
-// 	.then((data) => {
-// 		console.log(data);
-// 	});
 
 export default Information;
