c43731f470c59e020c06fab8f7ccdfdf2ed4b215
Ntshangase <simphiwe323i@gmail.com>
2024-05-22 08:07:18 +0200

    to be changed

--- src/Model/Information.js
+++ src/Model/Information.js
@@ -3,23 +3,24 @@ import { useState } from "react";
 function Information() {
 	const [currentData, setData] = useState([]);
 
-	//fetch data from API
-	console.log("This code must run once");
-	fetch("http://localhost:8080/users")
-		.then((response) => {
-			if (response.ok) {
-				console.log("status: " + response.status);
-				console.log(response);
-				return response.json();
-			} else {
-				console.log("An error occured while fetching response");
-			}
-		})
-		.then((data) => {
-			console.log(data);
-			setData(data);
-		});
-
+	
+		//fetch data from API
+		console.log("This code must run once");
+		fetch("http://localhost:8080/users")
+			.then((response) => {
+				if (response.ok) {
+					console.log("status: " + response.status);
+					console.log(response);
+					return response.json();
+				} else {
+					console.log("An error occured while fetching response");
+				}
+			})
+			.then((data) => {
+				console.log(data);
+				setData(data);
+			});
+	
 	return (
 		<>
 			<div>
