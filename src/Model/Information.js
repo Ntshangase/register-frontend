b6ced73a3ab57d1171c773911780c5eff65f7bf6
Ntshangase <simphiwe323i@gmail.com>
2024-05-23 07:05:16 +0200

    orettier missing

--- src/Model/Information.js
+++ src/Model/Information.js
@@ -1,26 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Information() {
 	const [currentData, setData] = useState([]);
 
-	
-		//fetch data from API
-		console.log("This code must run once");
-		fetch("http://localhost:8080/users")
-			.then((response) => {
-				if (response.ok) {
-					console.log("status: " + response.status);
-					console.log(response);
-					return response.json();
-				} else {
-					console.log("An error occured while fetching response");
-				}
-			})
-			.then((data) => {
-				console.log(data);
-				setData(data);
-			});
-	
+	//fetch data from API	http://localhost:8080/users
+	console.log("This code must run once");
+	useEffect(() => { fetch("https://dummyjson.com/users")
+		.then((response) => {
+			if (response.ok) {
+				console.log("status: " + response.status);
+				console.log(response);
+				return response.json();
+			} else {
+				console.log("An error occured while fetching response");
+			}
+		}
+		.then((data) => {
+			console.log(data.users);
+			setData(data.users);
+		})},[]);
+
 	return (
 		<>
 			<div>
@@ -28,8 +27,8 @@ function Information() {
 			</div>
 			<div>
 				{currentData.map((item) => (
-					<div key={item.name}>
-						<p>{item.name}</p>
+					<div key={item.id}>
+					<p>{item.firstName}</p>
 					</div>
 				))}
 			</div>
