b9b665a2d695b9a5d72a5c00a22754810b53e6fd
Ntshangase <simphiwe323i@gmail.com>
2024-05-27 05:40:40 +0200

    home page ui

--- src/View/Home.js
+++ src/View/Home.js
@@ -4,6 +4,14 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h2>Welcome to student register application</h2>
+      <div>
+        <button>Create</button>
+        <button>Read</button>
+        <button>Update</button>
+        <button>Delete</button>
+      </div>
+    </div>
   )
 }
