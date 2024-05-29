afee6c57c57ed4fcab4fc7be824398264b527501
Ntshangase <simphiwe323i@gmail.com>
2024-05-29 06:07:12 +0200

    record ui

--- src/View/Record.js
+++ src/View/Record.js
@@ -4,8 +4,29 @@ import Navbar from "../Model/Navbar";
 export default function Record() {
 	return (
 		<div>
-			<Navbar/>
+			<Navbar />
 			<h2>Add student to collection</h2>
+
+			<form>
+				<p>
+					<label>Name</label>
+					<input />
+				</p>
+				<p>
+					<label>Surname</label>
+					<input />
+				</p>
+				<p>
+					<label>Course</label>
+					<input />
+				</p>
+				<p>
+					<label>StudentId</label>
+					<input />
+				</p>
+
+				<button>Add</button>
+			</form>
 		</div>
 	);
 }
