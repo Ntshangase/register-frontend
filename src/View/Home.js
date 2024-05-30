f6772e4f1698fbd6fecd58315f60872e823275cb
Ntshangase <simphiwe323i@gmail.com>
2024-05-30 08:14:20 +0200

    style gang

--- src/View/Home.js
+++ src/View/Home.js
@@ -1,13 +1,14 @@
-import '../Styles/home.css';
-import React from 'react';
-import Navbar from '../Model/Navbar';
+import "../Styles/home.css";
+import React from "react";
+import Navbar from "../Model/Navbar";
 
 export default function Home() {
-
-  return (
-    <div>
-      <Navbar/>
-      <h2>Welcome to student register application</h2>
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<div id="home">
+				<h2>Welcome to student register application</h2>
+			</div>
+		</div>
+	);
 }
