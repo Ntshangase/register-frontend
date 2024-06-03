b90007ac8ea13359c8460bcf5af1a5a4fe73942b
Ntshangase <simphiwe323i@gmail.com>
2024-06-03 07:13:51 +0200

    dummy data

--- src/Model/Information.js
+++ src/Model/Information.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 function Information() {
 	const [currentData, setData] = useState([]);
 
-	//fetch data from API	https://dummyjson.com/users
-	useEffect(() => { fetch("http://localhost:8080/users")
+	//fetch data from API	http://localhost:8080/users
+	useEffect(() => { fetch("https://dummyjson.com/users")
 		.then((response) => {
 			if (response.ok) {
 				console.log("status: " + response.status);
@@ -15,8 +15,8 @@ function Information() {
 			}
 		})
 		.then((data) => {
-			console.log(data);
-			setData(data);
+			console.log(data.users);
+			setData(data.users); // dummyData.users
 		})},[]);
 
 	return (
@@ -25,11 +25,18 @@ function Information() {
 				<h1>Home renovations</h1>
 			</div>
 			<div>
-				{currentData.map((item) => (
+				{/* {currentData.map((item) => (
 					<div key={item.studentId}>
 						<p>StudentId: {item.studentId}</p>
 						<p>Student Name: {item.name} {item.surname}</p>
 						<p>Diploma in: {item.course}</p>
+					</div>
+				))} */}
+
+				{/**for dummy data */} 
+				{currentData.map(items => (
+					<div key={items.id}>
+						<p>{items.id}</p>
 					</div>
 				))}
 			</div>
