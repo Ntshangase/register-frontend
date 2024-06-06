8921da90854b1408410535d268721c6c2ee25bda
Ntshangase <simphiwe323i@gmail.com>
2024-06-06 02:49:37 +0200

    seeing flames

--- src/Model/Information.js
+++ src/Model/Information.js
@@ -1,29 +1,31 @@
 import { useState, useEffect } from "react";
-import Students from "../View/Students";
 
 function Information() {
 	const [currentData, setData] = useState([]);
 
 	//fetch data from API	http://localhost:8080/users
-	useEffect(() => { fetch("https://dummyjson.com/users")
-		.then((response) => {
-			if (response.ok) {
-				console.log("status: " + response.status);
-				console.log(response);
-				return response.json();
-			} else {
-				console.log("An error occured while fetching response");
-			}
-		})
-		.then((data) => {
-			console.log(data.users);
-			setData(data.users); // dummyData.users
-		})},[]);
+	useEffect(() => {
+		fetch("https://dummyjson.com/users")
+			.then((response) => {
+				if (response.ok) {
+					console.log("status: " + response.status);
+					console.log(response);
+					return response.json();
+				} else {
+					console.log("An error occured while fetching response");
+				}
+			})
+			.then((data) => {
+				console.log(data.users);
+				setData(data.users); // dummyData.users
+			});
+	}, []);
+
+	/**passing props to a child component */
+	//const props = { name: "Ntokozo", surname: "Biyela", age: 27 };
 
 	return (
 		<>
-			<Students props={100} />	{/**passing props to a child component */}
-
 			<div>
 				<h1>Home renovations</h1>
 			</div>
@@ -36,8 +38,8 @@ function Information() {
 					</div>
 				))} */}
 
-				{/**for dummy data */} 
-				{currentData.map(items => (
+				{/**for dummy data */}
+				{currentData.map((items) => (
 					<div key={items.id}>
 						<p>{items.id}</p>
 					</div>
--- src/View/Students.js
+++ src/View/Students.js
@@ -2,15 +2,29 @@
 
 import React from "react";
 import Navbar from "../Model/Navbar";
+import "../Model/Information";
 
-function Students(props) {
-	console.log(props);
+
+function Students() {
+	console.log("students");
+	console.log();
+	//objects
+	const testObject = {
+		name: "Simphiwe",
+		surname: "Ntshangase",
+		age: 23,
+	};
+
+	console.log(testObject);
+
 	return (
 		<div>
 			<Navbar />
 			<h2>Display student data</h2>
 
-			<div> {/**This has to be inside the thing, with key: value vibes */}
+			<div>
+				{" "}
+				{/**This has to be inside the thing, with key: value vibes */}
 				<div>
 					<p>studentName</p>
 					<p>studentSurname</p>
